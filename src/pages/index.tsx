bf410842efefa33d6fc3fe0502b7405022889a17
Obed Santos <debosotnas@hotmail.com>
2023-02-10 16:34:53 -0300

    Initial commit with basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sora Take Home</title>
+        <meta name="description" content="Sora Take Home App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+      </main>
+    </>
+  )
+}
